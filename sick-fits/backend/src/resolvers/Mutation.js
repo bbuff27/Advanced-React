8de43b5f0b079e0b73f10111c07a414253bfb792
Bill Buff <bbuff27@gmail.com>
2019-04-09 22:30:57 -0600

    Add createItem method to Mutation resolver

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,13 @@
 const Mutation = {
-  // createDogs(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog
-  // }
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item;
+  }
 };
 
 module.exports = Mutation;
