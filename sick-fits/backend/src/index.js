c54e2aa51bd40ff2117f9268b97a0040dbea7697
Bill Buff <bbuff27@gmail.com>
2019-04-09 21:56:54 -0600

    Build schema for mutation and query of items

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -4,6 +4,9 @@ const db = require('./db');
 
 const server = createServer();
 
+// TODO: Use express middleware to handle cookies (JWT)
+// TODO: Use express middleware to populate current user
+
 server.start(
   {
     cors: {
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutation = {};
+const Mutation = {
+  // createDogs(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog
+  // }
+};
 
 module.exports = Mutation;
